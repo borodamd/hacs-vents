c8d56776cc855ae11ddd6ac43bb633ff0dd76602
root <grati78@gmail.com>
2025-02-16 04:35:34 +0000

    Version 0.0.2

--- custom_components/hacs-vents/const.py
+++ custom_components/hacs-vents/const.py
@@ -1,6 +1,6 @@
 """Constants for the EcoVent_v2 integration."""
 
-DOMAIN = "ecovent_v2"
+DOMAIN = "hacs_vents"
 
 SERVICE_FILTER_TIMER_RESET = "filter_timer_reset"
 SERVICE_RESET_ALARMS = "reset_alarms"
